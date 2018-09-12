8c8dfeac6659f64840f82fe2a8f20d925911cdc9
HDCP <theaccountname@yahoo.com>
2018-09-12 06:48:59 +0500

    Launching recent command

--- shell.c
+++ shell.c
@@ -15,61 +15,69 @@ void init()
         char input[inputSize] = "\0";
         // memset(input, '\0', 100);
         fgets(input, sizeof(input), stdin);
-        if (strncmp(input, "exit", 4) == 0)
-        {
-            return;
-        }
-        else if (strncmp(input, "cd", 2) == 0)
-        {
-            int i = 0;
-            int pathLength = 0;
-            int cdLength = sizeof("cd");
+        runCommand(input);
+    }
+}
 
-            for (i = cdLength; input[i] != '\0'; i++)
+void runCommand(char input[])
+{
+    if (strncmp(input, "exit", 4) == 0)
+    {
+        return;
+    }
+    else if (strncmp(input, "cd", 2) == 0)
+    {
+        int i = 0;
+        int pathLength = 0;
+        int cdLength = sizeof("cd");
+
+        for (i = cdLength; input[i] != '\0'; i++)
+        {
+            if (input[i] == 32)
             {
-                if (input[i] == 32)
-                {
-                    printError("path can not have spaces in them");
-                    break;
-                }
-                pathLength++;
+                printError("path can not have spaces in them");
+                break;
             }
-            char *directoryPath = (char *)malloc(pathLength * sizeof(char));
-            strncpy(directoryPath, input + cdLength, pathLength - 1);
-            // printf("Path is %s\n", directoryPath);
-            if (chdir(directoryPath) == -1)
-            {
-                printError("invalid directory path");
-            }
+            pathLength++;
+        }
+        char *directoryPath = (char *)malloc(pathLength * sizeof(char));
+        strncpy(directoryPath, input + cdLength, pathLength - 1);
+        // printf("Path is %s\n", directoryPath);
+        if (chdir(directoryPath) == -1)
+        {
+            printError("invalid directory path");
+        }
+        addToHistory(input);
+    }
+    else if (strncmp(input, "history", 6) == 0)
+    {
+        if (strncmp(input, "history -c", 9) == 0)
+        {
+            historyCount = -1;
+        }
+        addToHistory(input);
+        printHistory();
+    }
+    else if (strncmp(input, "!!", 2) == 0)
+    {
+        launchRecentCommand();
+    }
+    else
+    {
+        //When the other command is entered
+        if (!(strcmp(input, "\n") == 0))
+        {
             addToHistory(input);
-        }
-        else if (strncmp(input, "history", 6) == 0)
-        {
-            if (strncmp(input, "history -c", 9) == 0)
-            {
-                historyCount = -1;
-            }
-            addToHistory(input);
-            printHistory();
-        }
-        else if (strncmp(input, "!!", 2) == 0)
-        {
-            launchRecentCommand();
-        }
-        else
-        {
-            //When the other command is entered
-            if (!(strcmp(input, "\n") == 0))
-            {
-                addToHistory(input);
-            }
         }
     }
 }
 void launchRecentCommand()
 {
-    if (historyCount != -1)
-        printf("Recent command was %s\n", history[historyCount - 1]);
+    if (historyCount != 0)
+    {
+        // printf("Recent command was %s\n", history[historyCount - 1]);
+        runCommand(history[historyCount - 1]);
+    }
     else
         printError("there is no commands in the history");
 }
--- shell.h
+++ shell.h
@@ -5,10 +5,13 @@
 int historyLimit = 100;
 char *history[100];
 int historyCount = 0;
+
 void init();
+void runCommand(char input[]);
 void printError(char *message);
 void addToHistory(char *input);
 void printHistory();
 void checkHistoryLimit();
 void launchRecentCommand();
+
 #endif //SHELL_H
